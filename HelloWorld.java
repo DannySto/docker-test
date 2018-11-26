5d3652d68c9065fa84e416440aa98d57cafc82ff
Danny Stompe <danny@Dannys-MacBook-Pro.local>
2018-11-26 12:27:42 +0100

    tet

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
         public static void main(String[] args)
         {
-                System.out.println("Bad Mother Fuckerssssss!!");
+                System.out.println("Bad Mother Fuckerssss!!");
         }
 }
